2638c1ffa3220ffaf53d2d3bf1df6b85f408822b
gonc.jonathan@gmail.com <gonc.jonathan@gmail.com@112be65d-5468-7f71-cc23-e784e3fe72f1>
2013-10-01 21:59:59 +0000

    Script d'intialisation de la base de données et entités Java

---
+++ trunk/src/main/java/com/emn/fila2/hujoke/association/model/Product.java
@@ -0,0 +1,56 @@
+package com.emn.fila2.hujoke.association.model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@NamedQuery(name="Product.findAll", query="SELECT p FROM Product p")
+public class Product implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String code;
+
+	private String name;
+
+	private BigDecimal price;
+
+	private int stock;
+
+	public Product() {
+	}
+
+	public String getCode() {
+		return this.code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public int getStock() {
+		return this.stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+}
--- trunk/src/main/java/com/emn/fila2/hujoke/association/model/User.java
+++ trunk/src/main/java/com/emn/fila2/hujoke/association/model/User.java
@@ -1,48 +1,88 @@
 package com.emn.fila2.hujoke.association.model;
 
-public class User {
-	private String login;
-	private String password;
-	private String lastName;
-	private String firstName;
-	private String street;
-	private String zipCode;
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+@Table(name="\"USER\"")
+@NamedQuery(name="User.findAll", query="SELECT u FROM User u")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	private String city;
-	
-	public String getLogin() {
-		return login;
-	}
-	
-	public void setLogin(String login) {
-		this.login = login;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
-	public void setPassword(String password) {
-		this.password = password;
+
+	@Column(name="FIRST_NAME")
+	private String firstName;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name="LAST_NAME")
+	private String lastName;
+
+	private String login;
+
+	private String password;
+
+	private String street;
+
+	@Column(name="ZIP_CODE")
+	private String zipCode;
+
+	public User() {
 	}
 
-	public String getLastName() {
-		return lastName;
+	public String getCity() {
+		return this.city;
 	}
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	public void setCity(String city) {
+		this.city = city;
 	}
-	
+
 	public String getFirstName() {
-		return firstName;
+		return this.firstName;
 	}
 
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLogin() {
+		return this.login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public String getStreet() {
-		return street;
+		return this.street;
 	}
 
 	public void setStreet(String street) {
@@ -50,18 +90,11 @@ public class User {
 	}
 
 	public String getZipCode() {
-		return zipCode;
+		return this.zipCode;
 	}
 
 	public void setZipCode(String zipCode) {
 		this.zipCode = zipCode;
 	}
 
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
 }
