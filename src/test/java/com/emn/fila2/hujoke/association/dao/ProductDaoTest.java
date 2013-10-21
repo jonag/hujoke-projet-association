2fd86d67c9db191adeb57402fc5bd10fcf16b7d5
clement.guyonnet@gmail.com <clement.guyonnet@gmail.com@112be65d-5468-7f71-cc23-e784e3fe72f1>
2013-10-21 20:55:09 +0000

    test dao product

---
+++ src/test/java/com/emn/fila2/hujoke/association/dao/ProductDaoTest.java
@@ -0,0 +1,32 @@
+package com.emn.fila2.hujoke.association.dao;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.*;
+
+import com.emn.fila2.hujoke.association.model.Product;
+
+public class ProductDaoTest {
+
+	private ProductDao dao;
+	
+	@Before
+	public void setUp(){
+		dao = new ProductDao();
+	}
+	
+	@Test
+	public void testFindAll(){
+		List<Product> products = dao.findAll();
+		assertTrue(products.size() == 1);
+	}
+	
+	@Test
+	public void testFind(){
+		Product product = dao.find("productTestFind");
+		assertNotNull(product);
+	}
+	
+}
